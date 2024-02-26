056fdeae3c54a3194e23d76688bc38832c78e0fa
tlkahn <joshgy@gmail.com>
2024-02-26 18:10:31 +0800

    last commit

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
-use filter_org_txt::load_patterns_from_toml;
+use filter_org_txt::{apply_patterns, load_patterns_from_toml};
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 
-fn main() {
-    let file_path = "/Users/toeinriver/Documents/pandoc-server-py/patterns.toml";
-    match load_patterns_from_toml(file_path) {
-        Ok(patterns) => {
-            for pattern in patterns {
-                println!("pattern: {:?}", pattern);
-            }
-        }
-        Err(e) => eprintln!("Error loading patterns: {}", e),
-    }
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let patterns_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("patterns.toml");
+    let patterns = load_patterns_from_toml(patterns_path.to_str().unwrap())?;
+
+    let input_file_path = env::args().nth(1).expect("Missing input file path");
+    let text = std::fs::read_to_string(&input_file_path)?;
+
+    let updated_text = apply_patterns(&patterns, &text);
+
+    let output_file_path = env::args()
+        .nth(2)
+        .unwrap_or_else(|| input_file_path.clone());
+    let mut output_file = File::create(output_file_path)?;
+    output_file.write_all(updated_text.as_bytes())?;
+
+    Ok(())
 }
